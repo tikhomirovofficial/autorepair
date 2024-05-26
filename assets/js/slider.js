a5a9db4c4c1ed08d8e74c276b1e3e60d472797b2
tikhomirovofficial <arteom.tixomirow@yandex.ru>
2024-05-26 23:23:58 +0300

    slider connected, promo section

---
+++ assets/js/slider.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper(".welcome", {
+    spaceBetween: 30,
+    effect: "fade",
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
